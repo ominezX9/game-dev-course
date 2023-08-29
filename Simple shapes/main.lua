97c8bd3d698a2f4a7d8f0b16dd6e9edc903b6080
David Agu <david.agu@iisysgroup.com>
2023-08-29 12:59:34 +0100

    Simple Init

---
+++ Simple shapes/main.lua
@@ -0,0 +1,16 @@
+function love.load()
+    
+end
+
+function love.update(dt)
+    -- updates everyframe via deltatime
+
+
+
+end
+
+function love.draw()
+    -- this is used to draw a rectangle.
+    -- parameters(mode, x-pos, y-pos, width, height)
+    love.graphics.rectangle("fill", 200, 400, 400, 100)
+end
