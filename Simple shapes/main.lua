8a6673315ae5ffd609296ff261f9bccf0b8ee394
David Agu <david.agu@iisysgroup.com>
2023-08-29 13:53:10 +0100

    added a circel

--- Simple shapes/main.lua
+++ Simple shapes/main.lua
@@ -13,4 +13,5 @@ function love.draw()
     -- this is used to draw a rectangle.
     -- parameters(mode, x-pos, y-pos, width, height)
     love.graphics.rectangle("fill", 200, 400, 400, 100)
+    love.graphics.circle("fill", 300, 300, 50, 100)
 end
