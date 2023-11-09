0ec5ec6ebc3fcdcc53ca9b0dadc9168069e74314
X9 <david.agu@iisysgroup.com>
2023-11-09 13:21:00 +0100

    add: target game commit

---
+++ Target Game/main.lua
@@ -0,0 +1,30 @@
+function love.load()
+    -- executes at the start of the game
+    target = {}
+    -- target position
+    target.x = 300
+    target.y = 300
+    target.radius = 50
+
+    score = 0 
+    timer = 0
+
+    -- set game font and size
+    gameFont = love.graphics.newFont(40)
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    -- draw target 
+    love.graphics.setColor(1,0,0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    -- print text and set color to white
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 0, 0)
+end
