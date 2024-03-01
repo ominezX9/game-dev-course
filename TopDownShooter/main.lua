993d09cb463210b106c9cab811e6160f90ce14ea
X9 <david.agu@iisysgroup.com>
2024-03-01 14:15:55 +0100

    update: player position, frame rate and movement

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -6,12 +6,30 @@ function love.load()
     sprites.player = love.graphics.newImage('sprites/player.png')
     sprites.zombie = love.graphics.newImage('sprites/zombie.png')
 
+    player = {}
+    player.x = love.graphics.getWidth() / 2
+    player.y = love.graphics.getHeight() / 2
+    player.speed = 3
+
 
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed * dt
+    end
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed * dt
+    end
+    if love.keyboard.isDown("s") then
+        player.y = player.y + player.speed * dt
+    end
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed * dt
+    end
 end
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
+    love.graphics.draw(sprites.player, player.x, player.y) -- draw sprite
 end
